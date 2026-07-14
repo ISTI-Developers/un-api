0993ff1e41e4d579898ec4e3d0bc6fc390d70b7b
ISTI-Developers <isti@unmg.com.ph>
2026-07-14 14:59:31 +0800

    invoice minor changes 2

--- src/controller/jv.controller.ts
+++ src/controller/jv.controller.ts
@@ -230,44 +230,44 @@ export const JVController = {
     }
   },
   async getRevenueByInvoice(req: Request, res: Response) {
-    try {
-      const rawInvoiceNumbers = String(req.query.cInvNo ?? "");
-      const invoiceNumbers = rawInvoiceNumbers
-        .split(",")
-        .map((invoiceNumber) => invoiceNumber.trim())
-        .filter(Boolean);
+    const rawInvoiceNumbers = String(req.query.cInvNo ?? "").trim();
 
-      const uniqueInvoiceNumbers = [...new Set(invoiceNumbers)];
-      if (uniqueInvoiceNumbers.length === 0) {
-        throw new Error("cInvNo is required.");
-      }
-      const invoiceList = uniqueInvoiceNumbers
-        .map((invoiceNumber) => {
-          const escapedInvoiceNumber = invoiceNumber.replace(/'/g, "''");
-          return `'${escapedInvoiceNumber}'`;
-        })
-        .join(",");
-      const escapedInvoiceList = invoiceList.replace(/'/g, "''");
-      const remoteQuery = `
-      SELECT *
-      FROM OPENQUERY(UNLIVE_LINK, '
+    const invoiceNumbers = rawInvoiceNumbers
+      .split(",")
+      .map((invoiceNumber) => invoiceNumber.trim())
+      .filter(Boolean);
+
+    const uniqueInvoiceNumbers = [...new Set(invoiceNumbers)];
+
+    if (uniqueInvoiceNumbers.length === 0) {
+      throw new Error("cInvNo is required.");
+    }
+
+    const invoiceList = uniqueInvoiceNumbers
+      .map((invoiceNumber) => {
+        const escapedInvoiceNumber = invoiceNumber.replace(/'/g, "''");
+        return `'${escapedInvoiceNumber}'`;
+      })
+      .join(",");
+
+    const escapedInvoiceList = invoiceList.replace(/'/g, "''''");
+
+    const query = `
+    SELECT *
+    FROM OPENQUERY(UNLIVE_LINK, '
       SELECT *
       FROM UN_LIVE.dbo.Get_JV_Revenue_Invoice_List(
-        '${escapedInvoiceList}'
-      )')`;
-      const escapedRemoteQuery = remoteQuery.replace(/'/g, "''");
-      const query = `
-      SELECT *
-      FROM OPENQUERY(
-        UNLIVE_LINK,
-        '${escapedRemoteQuery}'
+        ''${escapedInvoiceList}''
       )
-    `;
+    ')
+  `;
 
+    try {
       const result = await db.query(query);
       send(res).ok(result);
     } catch (error) {
       send(res).error(error);
     }
   },
+
 };
