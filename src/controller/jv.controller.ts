311802e9f0db3e51f9f0057aa1231b42ad806143
ISTI-Developers <isti@unmg.com.ph>
2026-07-15 10:24:44 +0800

    Sample

--- src/controller/jv.controller.ts
+++ src/controller/jv.controller.ts
@@ -230,29 +230,12 @@ export const JVController = {
     }
   },
   async getRevenueByInvoice(req: Request, res: Response) {
-    const cInvNo = String(req.query.cInvNo ?? "");
-
-    if (!cInvNo) {
-      throw new Error("cInvNo is required.");
-    }
-
-    const invoiceList = cInvNo
-      .split(",")
-      .map((invoiceNumber) => `'${invoiceNumber.replace(/'/g, "''")}'`)
-      .join(",");
-
-    const escapedInvoiceList = invoiceList.replace(/'/g, "''''");
-
     const query = `
     SELECT *
     FROM OPENQUERY(UNLIVE_LINK, '
       SELECT *
-      FROM UN_LIVE.dbo.Get_JV_Revenue_Invoice_List(
-        ''${escapedInvoiceList}''
-      )
-    ')
+      FROM UN_LIVE.dbo.Get_JV_Revenue_Invoice_List('SI-2608900','SI-2608901')')
   `;
-
     try {
       const result = await db.query(query);
       send(res).ok(result);
@@ -260,6 +243,5 @@ export const JVController = {
       send(res).error(error);
     }
   },
-
 
 };
